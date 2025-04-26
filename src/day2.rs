c164e348283bc8bfa7e45e7b71eb765417e052c2
Maitreya Venkataswamy <maitreyakv@gmail.com>
2025-04-26 12:02:51 -0400

    feat: adds day 3 solution

--- src/day2.rs
+++ src/day2.rs
@@ -105,17 +105,7 @@ mod test {
 
     #[test]
     fn test_part_2() {
-        assert_eq!(
-            part_2(
-                r#"7 6 4 2 1
-1 2 7 8 9
-9 7 6 2 1
-1 3 2 4 5
-8 6 4 4 1
-1 3 6 7 9"#
-            ),
-            4
-        )
+        assert_eq!(part_2(INPUT), 418)
     }
 }
 
